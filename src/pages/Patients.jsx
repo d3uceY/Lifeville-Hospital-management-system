91fa3fa8c6f482c90cf2372efdfcd1616c544b41
Onyekwelu "Deuce" Jesse <onyekwelujesse1234@gmail.com>
2025-03-11 00:34:43 +0100

    @update/added table that displays patient data

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,6 +1,9 @@
 "use client"
 
 import * as React from "react"
+import { useEffect } from "react"
+//api function to get patients
+import { getRegisteredPatients } from "../providers/ApiProviders"
 import {
   flexRender,
   getCoreRowModel,
@@ -32,13 +35,13 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
-const data = [
-  { id: "m5gr84i9", amount: 316, status: "success", email: "ken99@example.com" },
-  { id: "3u1reuv4", amount: 242, status: "success", email: "Abe45@example.com" },
-  { id: "derv1ws0", amount: 837, status: "processing", email: "Monserrat44@example.com" },
-  { id: "5kma53ae", amount: 874, status: "success", email: "Silas22@example.com" },
-  { id: "bhqecj4p", amount: 721, status: "failed", email: "carmella@example.com" },
-]
+// const data = [
+//   { id: "m5gr84i9", amount: 316, status: "success", email: "ken99@example.com" },
+//   { id: "3u1reuv4", amount: 242, status: "success", email: "Abe45@example.com" },
+//   { id: "derv1ws0", amount: 837, status: "processing", email: "Monserrat44@example.com" },
+//   { id: "5kma53ae", amount: 874, status: "success", email: "Silas22@example.com" },
+//   { id: "bhqecj4p", amount: 721, status: "failed", email: "carmella@example.com" },
+// ]
 
 const columns = [
   {
@@ -60,30 +63,80 @@ const columns = [
     enableSorting: false,
     enableHiding: false,
   },
+  // {
+  //   accessorKey: "status",
+  //   header: "Status",
+  //   cell: ({ row }) => <div className="capitalize">{row.getValue("status")}</div>,
+  // },
   {
-    accessorKey: "status",
-    header: "Status",
-    cell: ({ row }) => <div className="capitalize">{row.getValue("status")}</div>,
-  },
-  {
-    accessorKey: "email",
+    accessorKey: "hospital_number",
     header: ({ column }) => (
       <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-        Email
+        Hospital number
         <ArrowUpDown />
       </Button>
     ),
-    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+    cell: ({ row }) => <div className="lowercase">{row.getValue("hospital_number")}</div>,
   },
   {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Amount</div>,
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"))
-      const formatted = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(amount)
-      return <div className="text-right font-medium">{formatted}</div>
-    },
+    accessorKey: "surname",
+    header: ({ column }) => (
+      <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+        Surname
+        <ArrowUpDown />
+      </Button>
+    ),
+    cell: ({ row }) => <div className="lowercase">{row.getValue("surname")}</div>,
   },
+  {
+    accessorKey: "first_name",
+    header: ({ column }) => (
+      <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+        First name
+        <ArrowUpDown />
+      </Button>
+    ),
+    cell: ({ row }) => <div className="lowercase">{row.getValue("first_name")}</div>,
+  },
+  {
+    accessorKey: "sex",
+    header: ({ column }) => (
+      <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+        Sex
+        <ArrowUpDown />
+      </Button>
+    ),
+    cell: ({ row }) => <div className="lowercase">{row.getValue("sex")}</div>,
+  },
+  {
+    accessorKey: "date_of_birth",
+    header: ({ column }) => (
+      <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+        date_of_birth
+        <ArrowUpDown />
+      </Button>
+    ),
+    cell: ({ row }) => <div className="lowercase">{row.getValue("date_of_birth")}</div>,
+  },
+  {
+    accessorKey: "phone_number",
+    header: ({ column }) => (
+      <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+        phone_number
+        <ArrowUpDown />
+      </Button>
+    ),
+    cell: ({ row }) => <div className="lowercase">{row.getValue("phone_number")}</div>,
+  },
+  // {
+  //   accessorKey: "amount",
+  //   header: () => <div className="text-right">Amount</div>,
+  //   cell: ({ row }) => {
+  //     const amount = parseFloat(row.getValue("amount"))
+  //     const formatted = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(amount)
+  //     return <div className="text-right font-medium">{formatted}</div>
+  //   },
+  // },
   {
     id: "actions",
     enableHiding: false,
@@ -117,9 +170,10 @@ export default function Patients() {
   const [columnFilters, setColumnFilters] = React.useState([])
   const [columnVisibility, setColumnVisibility] = React.useState({})
   const [rowSelection, setRowSelection] = React.useState({})
+  const [patients, setPatients] = React.useState([])
 
   const table = useReactTable({
-    data,
+    data:patients,
     columns,
     onSortingChange: setSorting,
     onColumnFiltersChange: setColumnFilters,
@@ -132,13 +186,28 @@ export default function Patients() {
     state: { sorting, columnFilters, columnVisibility, rowSelection },
   })
 
+  useEffect(() => {
+    const getPatients = async () => {
+      try {
+        const response = await getRegisteredPatients();
+        setPatients(response)
+        console.log(response)
+      } catch (err) {
+        console.error(response, err)
+      }
+    }
+
+    getPatients()
+  }, [])
+
+
   return (
     <div className="w-full">
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter emails..."
-          value={table.getColumn("email")?.getFilterValue() || ""}
-          onChange={(event) => table.getColumn("email")?.setFilterValue(event.target.value)}
+          placeholder="Filter surname..."
+          value={table.getColumn("surname")?.getFilterValue() || ""}
+          onChange={(event) => table.getColumn("surname")?.setFilterValue(event.target.value)}
           className="max-w-sm"
         />
         <DropdownMenu>
