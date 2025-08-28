9cf8f430ff9aabd94f6b4e2a087947d422c650f3
Onyekwelu "Deuce" Jesse <onyekwelujesse1234@gmail.com>
2025-08-29 00:12:01 +0100

    fix: use hashrouter intstead of browser router for react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import SuspenseFallback from './components/loader/SuspenseFallback';
 import ProtectedRoute from './ProtectedRoute';
